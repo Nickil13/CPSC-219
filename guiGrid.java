927ec710887fe3c059a5d57eb7cd9db42b954aad
rbschaaf <35942400+rbschaaf@users.noreply.github.com>
2018-03-02 20:32:54 -0700

    Update guiGrid.java
    I had to create a second label in order to giver error messaging for invalid room number entries on both the start screen and grid screen.

--- guiGrid.java
+++ guiGrid.java
@@ -19,7 +19,7 @@ public class guiGrid extends Application {
   * There needs to be an argument for the new map creation to work with the
   * changes I made, ideally this would come from a button click that would
   * take in a room number - Dayan 22 Feb 2018
-  * Last updated Feb 28 by Nicki.
+  * Last updated March 2 20:30 by Riley.
   */
 
   private int rowNum = 14;
@@ -27,6 +27,8 @@ public class guiGrid extends Application {
   private Map map1 = new Map();
   private GridPane gridPane = new GridPane();
   private int roomNumbers = 0;
+  private Label invalidEntry = new Label ("");
+  private Label invalidEntry2 = new Label("");
 
 
   public void makeGUI(int[][] aGrid, GridPane aGridPane){
@@ -80,8 +82,42 @@ public class guiGrid extends Application {
     }
     // place start and destination markers
 
+
   }
 
+  //check if entered valid start
+  public boolean isValidStartRoom (int aStartRoom){
+    boolean isValidStart = true;
+    if(aStartRoom != 25 && aStartRoom != 150&&  aStartRoom != 151&&
+    aStartRoom != 152&&  aStartRoom != 159&& aStartRoom != 160&&
+    aStartRoom != 161&&  aStartRoom != 162&& aStartRoom != 163&&
+    aStartRoom != 164&&  aStartRoom != 250&&  aStartRoom != 251&&
+    aStartRoom != 252&&  aStartRoom != 259&& aStartRoom != 260&&
+    aStartRoom != 261&&  aStartRoom != 262&& aStartRoom != 263&&
+    aStartRoom != 264){
+      System.out.println("Please enter a valid start room. Example: 162 or 164");
+      invalidEntry.setText("Please enter a valid start room. Example: 162 or 164");
+      invalidEntry2.setText("Please enter a valid start room. Example: 162 or 164");
+      isValidStart = false;
+    }return isValidStart;
+  }
+
+  //check if entered valid destination
+  public boolean isValidDestRoom (int aStartRoom){
+    boolean isValidDest = true;
+    if(aStartRoom != 25 && aStartRoom != 150&&  aStartRoom != 151&&
+    aStartRoom != 152&&  aStartRoom != 159&& aStartRoom != 160&&
+    aStartRoom != 161&&  aStartRoom != 162&& aStartRoom != 163&&
+    aStartRoom != 164&&  aStartRoom != 250&&  aStartRoom != 251&&
+    aStartRoom != 252&&  aStartRoom != 259&& aStartRoom != 260&&
+    aStartRoom != 261&&  aStartRoom != 262&& aStartRoom != 263&&
+    aStartRoom != 264){
+      System.out.println("Please enter a valid end room. Example: 151 or 161");
+      invalidEntry.setText("Please enter a valid end room. Example: 151 or 161");
+      invalidEntry2.setText("Please enter a valid end room. Example: 151 or 161");
+      isValidDest = false;
+    }return isValidDest;
+  }
 
   @Override
   public void start(Stage primaryStage){
@@ -105,7 +141,7 @@ public class guiGrid extends Application {
 
     VBox startVBox = new VBox(10);
     startVBox.getChildren().addAll(appTitle, buildingText, startText,
-    destText, startButton);
+    destText, invalidEntry2, startButton);
 
 
     //add all boxes to borderPane1
@@ -135,15 +171,19 @@ public class guiGrid extends Application {
     TextField enterStartRoom = new TextField("Enter the start room");
     TextField enterDestRoom= new TextField("Enter destination room");
 
+
     // new submit button for scene 2
     Button submitB = new Button("Submit");
     //handle when submit button (in scene 2) is clicked
     submitB.setOnAction(new EventHandler<ActionEvent>(){
       public void handle(ActionEvent event){
+        invalidEntry.setText("");
         String buildingSubmit = "Taylor Family Digital Library";
         int roomNumberSub1 = Integer.parseInt(enterStartRoom.getText());
         int roomNumberSub2 = Integer.parseInt(enterDestRoom.getText());
         FloorPlans updatedPlan2 = new FloorPlans();
+        isValidStartRoom(roomNumberSub1);
+        isValidDestRoom(roomNumberSub2);
         updatedPlan2.setGrid("Taylor Family Digital Library",
         roomNumberSub2);
         map1.setCurrentFloorPlan(updatedPlan2);
@@ -164,7 +204,7 @@ public class guiGrid extends Application {
 
     VBox topVBox = new VBox(15);
     topRow.getChildren().addAll(buildingDropDown, enterStartRoom,
-    enterDestRoom, submitB);
+    enterDestRoom, submitB, invalidEntry);
     topVBox.getChildren().addAll(topRow2,topRow);
     // set the alignment of the grid pane
     gridPane.setAlignment(Pos.CENTER);
@@ -179,11 +219,15 @@ public class guiGrid extends Application {
     //handle when start button (in scene 1) is clicked
     startButton.setOnAction(new EventHandler<ActionEvent>(){
       public void handle(ActionEvent event){
+        invalidEntry2.setText("");
         String building2 = buildingText.getText();
+        System.out.println(building2);
         int roomNumber1 = Integer.parseInt(startText.getText());
         int roomNumber2 = Integer.parseInt(destText.getText());
         FloorPlans updatedPlan = new FloorPlans();
-        updatedPlan.setGrid("Taylor Family Digital Library", roomNumber2);
+
+        if(isValidStartRoom(roomNumber1) && isValidDestRoom(roomNumber2)){
+        updatedPlan.setGrid(building2, roomNumber2);
         map1.setCurrentFloorPlan(updatedPlan);
 
         // add markers for the destination (yellow)
@@ -194,9 +238,10 @@ public class guiGrid extends Application {
         map1.placeStart(testPath.getStartRow(),testPath.getStartCol());
 
         testPath.createPath();
+        invalidEntry.setText("");
         // Create the GUI for the map
         makeGUI(updatedPlan.getGrid(),gridPane);
-        primaryStage.setScene(scene2);
+        primaryStage.setScene(scene2);}
       }
     });
 
